b18e51f40c9f0cfb373efd9ed2cd662cf344d4d9
Sebastian Faure <sfaure@altomovup.com>
2022-03-29 20:40:02 -0300

    test pipelines 3

--- create_pipeline.py
+++ create_pipeline.py
@@ -5,31 +5,32 @@ import os
 import json
 from pygments import highlight, lexers, formatters
 import glob
-
+NAME = "NAME"
+GROUP = "GROUP"
 
 def get_git_url():
     repo = Repo()
-    return repo.remotes.origin.url, False
+    return repo.remotes.origin.url, None
 
 
 def get_image_tag():
     repo = Repo()
     tag_name = repo.remotes.origin.url.split(
         "@gitlab.grupoalto.com:altochecks/")[-1].strip('.git')
-    return tag_name, False
+    return tag_name, None
 
 
 def get_pipeline_name():
     repo = Repo()
     pipeline_name = repo.remotes.origin.url.split('/')[-1].strip('.git')
-    return pipeline_name, True
+    return pipeline_name, "NAME"
 
 
 default_values = {
-    'PIPELINE_GROUP': lambda: (None, False),
+    'PIPELINE_GROUP': lambda: (None, "GROUP"),
     'PIPELINE_NAME': get_pipeline_name,
     'GIT_URL': get_git_url,
-    'NEXUS': lambda: ('nexus.altoalliance.com:8086', False),
+    'NEXUS': lambda: ('nexus.altoalliance.com:8086', None),
     'IMAGEN_TAG': get_image_tag
 }
 
@@ -42,13 +43,16 @@ if __name__ == "__main__":
         variables = re.findall(r'{{(.*)}}', file_content)
         values = {}
         for variable in variables:
-            default, is_name = default_values.get(variable, lambda: None)()
+            default, value_type = default_values.get(variable, lambda: None)()
             while(True):
                 value = input(
                     f"insert value for {variable} [{default}]:\n") or default
-                if is_name:
+                if value_type == GROUP:
+                    group_path = os.path.join(script_dir, value)
+
+                if value_type == NAME:
                     output_filename = f"{value}.gopipeline.json"
-                    output_path = os.path.join(script_dir, output_filename)
+                    output_path = os.path.join(group_path, output_filename)
                     exist_path = os.path.join(script_dir, "*", output_filename)
                     print(exist_path)
                     if glob.glob(exist_path):
@@ -77,6 +81,8 @@ if __name__ == "__main__":
             save = input(
                 "Do you want to save pipeline?[Y/n]:\n",).lower() or default_save
             if save in ['yes', 'y', 'si', 's']:
+                if not os.path.exists(group_path):
+                    os.mkdir(group_path)
                 with open(output_path, 'w') as output_file:
                     output_file.write(output)
                     break
