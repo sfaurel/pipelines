ce14669da56765bc085b69c9214bacdcb4105734
Sebastian Faure <sfaure@altomovup.com>
2022-04-12 12:18:53 -0400

    test

--- create_pipeline.py
+++ create_pipeline.py
@@ -7,6 +7,9 @@ from pygments import highlight, lexers, formatters
 import glob
 NAME = "NAME"
 GROUP = "GROUP"
+WARN = "\033[1;33"
+DEFAULT = "\033[0m"
+
 
 def get_git_url():
     repo = Repo()
@@ -23,11 +26,11 @@ def get_image_tag():
 def get_pipeline_name():
     repo = Repo()
     pipeline_name = repo.remotes.origin.url.split('/')[-1].strip('.git')
-    return pipeline_name, "NAME"
+    return pipeline_name, NAME
 
 
 default_values = {
-    'PIPELINE_GROUP': lambda: (None, "GROUP"),
+    'PIPELINE_GROUP': lambda: (None, GROUP),
     'PIPELINE_NAME': get_pipeline_name,
     'GIT_URL': get_git_url,
     'NEXUS': lambda: ('nexus.altoalliance.com:8086', None),
@@ -42,22 +45,58 @@ if __name__ == "__main__":
         template = Template(file_content)
         variables = re.findall(r'{{(.*)}}', file_content)
         values = {}
+
+        while(True):
+            default, value_type = default_values.get('PIPELINE_GROUP', lambda: (None, None))()
+            value = input(f"insert value for PIPELINE_GROUP [{default}]:\n") or default
+            if value:
+                group_path = os.path.join(script_dir, value)
+                values['PIPELINE_GROUP'] = value
+                break
+            else:
+                if default:
+                    group_path = os.path.join(script_dir, value)
+                    values['PIPELINE_GROUP'] = default
+                    break
+                else:
+                    print(f"{WARN}mplease insert a value{DEFAULT}")
+                    continue
+
+        while(True):
+            default, value_type = default_values.get('PIPELINE_NAME', lambda: None)()
+            value = input(
+                f"insert value for PIPELINE_NAME [{default}]:\n") or default
+            output_filename = f"{value}.gopipeline.json"
+            output_path = os.path.join(group_path, output_filename)
+            exist_path = os.path.join(script_dir, "*", output_filename)
+            if glob.glob(exist_path):
+                print(f"{WARN}mpipeline name already exist, please insert anotherone{DEFAULT}")
+                continue
+            if value:
+                values['PIPELINE_NAME'] = value
+                break
+            else:
+                if default:
+                    values['PIPELINE_NAME'] = default
+                    break
+                else:
+                    print(f"{WARN}mplease insert a value{DEFAULT}")
+                    continue
+
+
+        
         for variable in variables:
             default, value_type = default_values.get(variable, lambda: None)()
             while(True):
-                value = input(
-                    f"insert value for {variable} [{default}]:\n") or default
+                # GROUP a NAME are defined first
                 if value_type == GROUP:
-                    group_path = os.path.join(script_dir, value)
+                    break
 
                 if value_type == NAME:
-                    output_filename = f"{value}.gopipeline.json"
-                    output_path = os.path.join(group_path, output_filename)
-                    exist_path = os.path.join(script_dir, "*", output_filename)
-                    print(exist_path)
-                    if glob.glob(exist_path):
-                        print("pipeline name already exist, please insert anotherone")
-                        continue
+                    break
+
+                value = input(
+                    f"insert value for {variable} [{default}]:\n") or default
 
                 if value:
                     values[variable] = value
@@ -67,19 +106,19 @@ if __name__ == "__main__":
                         values[variable] = default
                         break
                     else:
-                        print("please insert a value")
+                        print(f"{WARN}mplease insert a value{DEFAULT}")
                         continue
         output = template.render(**values)
 
         formatted_json = json.dumps(json.loads(output), indent=4)
         colorful_json = highlight(
             formatted_json, lexers.JsonLexer(), formatters.TerminalFormatter())
-        print("new pipeline was generated, check before save it")
+        print("new pipeline:")
         print(colorful_json)
         while(True):
             default_save = 'y'
             save = input(
-                "Do you want to save pipeline?[Y/n]:\n",).lower() or default_save
+                "Do you want to save generated pipeline?[Y/n]:\n",).lower() or default_save
             if save in ['yes', 'y', 'si', 's']:
                 if not os.path.exists(group_path):
                     os.mkdir(group_path)
@@ -89,5 +128,4 @@ if __name__ == "__main__":
             elif save in ['no', 'n']:
                 break
             else:
-                print(
-                    "Bad option, valid options are ('yes', 'y', 'si', 's', 'no', 'n'")
+                print(f"{WARN}mBad option, valid options are ('yes', 'y', 'si', 's', 'no', 'n'{DEFAULT}")
